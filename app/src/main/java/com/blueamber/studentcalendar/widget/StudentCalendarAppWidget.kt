393aa9d539fe20ae815de761d401728cad2a8aa7
Matthieu Bouquet <matthieu.bouquet@webwag.com>
2018-10-29 14:11:18 +0100

    Added : add first file for widget

---
+++ app/src/main/java/com/blueamber/studentcalendar/widget/StudentCalendarAppWidget.kt
@@ -0,0 +1,45 @@
+package com.blueamber.studentcalendar.widget
+
+import android.app.Service
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.widget.RemoteViews
+import com.blueamber.studentcalendar.R
+
+
+class StudentCalendarAppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    companion object {
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+            context.startService(Intent(context, UpdateService::class.java))
+        }
+    }
+
+    class UpdateService : Service() {
+
+        override fun onBind(intent: Intent?): IBinder? = null
+
+        override fun onCreate() {
+            val updateViews = buildUpdate(this)
+            val thisWidget = ComponentName(this, StudentCalendarAppWidget::class.java)
+            val manager = AppWidgetManager.getInstance(this)
+            manager.updateAppWidget(thisWidget, updateViews)
+        }
+
+        fun buildUpdate(context: Context): RemoteViews {
+            val views = RemoteViews(context.packageName, R.layout.student_calendar_app_widget)
+            return views
+        }
+    }
+}
+
