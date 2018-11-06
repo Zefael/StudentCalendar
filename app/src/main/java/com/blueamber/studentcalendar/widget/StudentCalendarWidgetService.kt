0e0f25eae27496fc12fa22db09e1c58c18801278
Matt <matthieu_bouquet@hotmail.fr>
2018-11-07 00:07:21 +0100

    wip : content provider

--- app/src/main/java/com/blueamber/studentcalendar/widget/StudentCalendarWidgetService.kt
+++ app/src/main/java/com/blueamber/studentcalendar/widget/StudentCalendarWidgetService.kt
@@ -7,24 +7,20 @@ import android.net.Uri
 import android.os.Binder
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
-import android.os.Binder.restoreCallingIdentity
-import android.os.Binder.clearCallingIdentity
 import com.blueamber.studentcalendar.R
-import com.blueamber.studentcalendar.models.Day
-import com.blueamber.studentcalendar.tools.DateUtil
+import com.blueamber.studentcalendar.models.Work
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import org.json.JSONArray
 
 
 class StudentCalendarWidgetService : RemoteViewsService() {
-    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory = StudentCalendarWidgetFactory(this.applicationContext, intent)
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory =
+        StudentCalendarWidgetFactory(this.applicationContext, intent)
 }
 
 class StudentCalendarWidgetFactory(val context: Context, val intent: Intent?) : RemoteViewsService.RemoteViewsFactory {
 
     private var cursor: Cursor? = null
-    private var data: List<Day>? = null
 
     override fun onCreate() {
         initCursor()
@@ -32,12 +28,12 @@ class StudentCalendarWidgetFactory(val context: Context, val intent: Intent?) : 
     }
 
     private fun initCursor() {
+        cursor?.close()
         val identityToken = Binder.clearCallingIdentity()
-        cursor = context.contentResolver.query(Uri.parse("content://com.blueamber.studentcalendar.provider/eventcalendar"),
+        cursor = context.contentResolver.query(
+            Uri.parse("content://com.blueamber.studentcalendar.provider/eventcalendar"),
             null, null, null, null)
         Binder.restoreCallingIdentity(identityToken)
-//        data = Gson().fromJson(cursor.toString(), object : TypeToken<List<Day>>() {}.type)
-        cursor?.close()
     }
 
     override fun getLoadingView(): RemoteViews = RemoteViews(context.packageName, R.layout.loading_view_app_widget)
@@ -52,16 +48,16 @@ class StudentCalendarWidgetFactory(val context: Context, val intent: Intent?) : 
 
     override fun getViewAt(position: Int): RemoteViews {
         val remoteViews = RemoteViews(context.packageName, R.layout.app_widget_item_event_list)
-        val day = data?.get(position)
-        if (day != null) {
-            val dayOfMonth = DateUtil.dayOfMonth(day.date.time).toString()
-            val dayOfWeek = DateUtil.dayOfWeek(day.date.time)
-            remoteViews.setTextViewText(R.id.item_event_date, dayOfMonth + "\n" + dayOfWeek)
-        }
+        cursor?.moveToPosition(position)
+//        val work: List<Work> = Gson().fromJson(cursor.toString(), object : TypeToken<List<Work>>() {}.type)
+//            val dayOfMonth = DateUtil.dayOfMonth().toString()
+//            val dayOfWeek = DateUtil.dayOfWeek(day.date.time)
+        remoteViews.setTextViewText(R.id.item_event_date, cursor?.getString(cursor?.getColumnIndex("date") ?: 0))
+//        remoteViews.setTextViewText(R.id.item_card_title, work.get(0).titre)
         return remoteViews
     }
 
-    override fun getCount(): Int = data?.size ?: 0
+    override fun getCount(): Int = cursor?.count ?: 0
 
     override fun getViewTypeCount(): Int = 1
 
